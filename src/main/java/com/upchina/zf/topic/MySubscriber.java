62fb899f2eaeb002506a0f09f687e023cb3b2d88
anjunli <anjunli@upchina.com>
2021-09-15 16:17:55 +0800

    持久性的TopicSubscriber

--- src/main/java/com/upchina/zf/topic/MySubscriber.java
+++ src/main/java/com/upchina/zf/topic/MySubscriber.java
@@ -1,14 +1,31 @@
 package com.upchina.zf.topic;
 
-import com.upchina.zf.MyListener;
+import com.upchina.zf.ptp.MyListener;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
+import java.io.IOException;
 
 /**
  * Created by anjunli on  2021/9/8
+ *
+ * 持久的Topic消息示例
+ *    1、给连接设置唯一的ID,创建DurableTopicSubscriber来订阅
+ *    2、设置完成后再start() Connection
+ *    3、消费者要先运行一次，相当于向服务注册这个消费者，然后运行生产者。这个时候无论消费者是否在线，都会接收到，下次连接的时候，会把没有收过的消息都接收下来。
+ *
+ *  注意：对于非持久性的Topic消息，则需要接收者保持运行状态，不然消息发送者发出的消息会接收不到。
  **/
 public class MySubscriber implements Runnable {
+
+    public String ID;
+
+    public MySubscriber() {
+    }
+
+    public MySubscriber(String ID) {
+        this.ID = ID;
+    }
 
     TopicConnection topicConnection;
     TopicSession topicSession;
@@ -19,21 +36,28 @@ public class MySubscriber implements Runnable {
         try {
             TopicConnectionFactory connectionFactory = new ActiveMQConnectionFactory("admin", "admin", "tcp://172.16.11.161:61616");
             topicConnection = connectionFactory.createTopicConnection();
-            topicConnection.start();
+            topicConnection.setClientID("Client_"+ID);
             topicSession = topicConnection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+
             //创建队列消息，topic
             Topic topic = topicSession.createTopic("topic_test");
+
             //创建消息订阅者
-            topicSubscriber = topicSession.createSubscriber(topic);
+//            topicSubscriber = topicSession.createSubscriber(topic);
+            /** 创建持久的TopicSubscriber*/
+            topicSubscriber = topicSession.createDurableSubscriber(topic, "Subscriber_"+ID);
+            topicConnection.start();
 
             //接受消息，会阻塞线程
-            Message message = topicSubscriber.receive();
+/*            Message message = topicSubscriber.receive();
             //输出消息
             String msg = ((TextMessage) message).getText();
-            System.out.println(msg);
+            System.out.println(msg);*/
 
-//            topicSubscriber.setMessageListener(new MyListener());
-        } catch (JMSException  e) {
+            topicSubscriber.setMessageListener(new MyListener());
+
+            System.in.read();
+        } catch (IOException | JMSException e) {
             e.printStackTrace();
         } finally {
             try {
--- src/main/java/com/upchina/zf/topic/TestSubscriber.java
+++ src/main/java/com/upchina/zf/topic/TestSubscriber.java
@@ -1,15 +1,15 @@
 package com.upchina.zf.topic;
 
-import com.upchina.zf.MyProducer;
-
 /**
  * Created by anjunli on  2021/9/8
  **/
 public class TestSubscriber {
     public static void main(String[] args) {
-        MySubscriber mySubscriber = new MySubscriber();
+        MySubscriber mySubscriber = new MySubscriber("101");
+        MySubscriber mySubscriber2 = new MySubscriber("102");
+        //利用线程来模拟多个消费者的情况
         Thread subscriber01 = new Thread(mySubscriber, "subscriber01");
-        Thread subscriber02 = new Thread(mySubscriber, "subscriber02");
+        Thread subscriber02 = new Thread(mySubscriber2, "subscriber02");
 
         subscriber01.start();
         subscriber02.start();
